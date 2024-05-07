2b0ad113fe083d6e78e505e021078d720c95645b
Javier Andrés Causil Martínez <jcausilmartinez@gmail.com>
2024-05-07 08:53:07 -0500

    Se ha agregado una función para reutilizar código

--- frontMaster/results-summary-component-main/js/main.js
+++ frontMaster/results-summary-component-main/js/main.js
@@ -1,10 +1,24 @@
-
-
+function changeText (listID, data) {
+    for (let i = 0; i <= listID.length; i++){
+            let id_0 = listID[i][0];
+            let id_1 = listID[i][1];
+            let id_2 = listID[i][2];
+            document.getElementById(id_0).src = `${data[i].icon}`;
+            document.getElementById(id_1).innerHTML = `${data[i].category}`;
+            document.getElementById(id_2).innerHTML = `${data[i].score}`;
+    }
+}
 
 const traerData = async ()  => {
 const xhttpr = new XMLHttpRequest()
 xhttpr.onload = function () {
     if (this.readyState == 4 && this.status == 200) {
+        const id = [
+            ['icon_reaction', 'reaction_category', 'score_reaction'], 
+            ['icon_memory', 'memory', 'score_memory' ], 
+            ['icon_verbal', 'verbal', 'score_verbal'],
+            ['icon_visual', 'visual', 'score_visual']
+        ]
         let data = JSON.parse(this.responseText)
         const yourResult = document.getElementById('yourResult');
         yourResult.innerHTML = `Your Result`
@@ -20,36 +34,7 @@ xhttpr.onload = function () {
         title_summary.innerHTML = `Summary`
         const button = document.getElementById('button');
         button.innerHTML = `Continue`;
-
-        const icon_reaction = document.getElementById('icon_reaction');
-        icon_reaction.src = `${data[0].icon}`
-        const reaction_category = document.getElementById('reaction_category')
-        reaction_category.innerHTML = `${data[0].category}`;
-        const score_reaction = document.getElementById('score_reaction');
-        score_reaction.innerHTML = `${data[0].score}`;
-        
-        
-        const icon_memory = document.getElementById('icon_memory');
-        icon_memory.src = `${data[1].icon}`
-        const memory = document.getElementById('memory')
-        memory.innerHTML = `${data[1].category}`;
-        const score_memory = document.getElementById('score_memory')
-        score_memory.innerHTML = `${data[1].score}`
-
-        const icon_verbal = document.getElementById('icon_verbal');
-        icon_verbal.src = `${data[2].icon}`
-        const verbal = document.getElementById('verbal')
-        verbal.innerHTML = `${data[2].category}`;
-        const score_verbal = document.getElementById('score_verbal')
-        score_verbal.innerHTML = `${data[2].score}`
-
-        const icon_visual = document.getElementById('icon_visual');
-        icon_visual.src = `${data[3].icon}`
-        const visual = document.getElementById('visual')
-        visual.innerHTML = `${data[3].category}`;
-        const score_visual = document.getElementById('score_visual')
-        score_visual.innerHTML = `${data[3].score}`;
-        
+        changeText(id, data)
     }
 }
 xhttpr.open('GET', './data.json', true)
